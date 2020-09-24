fbb7f452ba9f1512af235df4de4ee3f1a9ecd8db
Joel Brenstrum <joel.brenstrum@gmail.com>
2020-09-24 16:22:12 +1200

    Updates

--- precache-manifest.04fe694aa9afcd257cb0b1cdc5ff6905.js
+++ precache-manifest.04fe694aa9afcd257cb0b1cdc5ff6905.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7c5c331cc06e630976b10f3f8d6082e2",
+    "revision": "eec93181317805c60f28b4a77c922516",
     "url": "/material-icon-search/index.html"
   },
   {
-    "revision": "8738ce6c2c0bb664c809",
+    "revision": "252524b92a1a289d1047",
     "url": "/material-icon-search/static/css/main.0d17cae0.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/material-icon-search/static/js/2.2f51ea29.chunk.js"
   },
   {
-    "revision": "8738ce6c2c0bb664c809",
-    "url": "/material-icon-search/static/js/main.525fb0d8.chunk.js"
+    "revision": "252524b92a1a289d1047",
+    "url": "/material-icon-search/static/js/main.98747771.chunk.js"
   },
   {
     "revision": "a1d865100b7dfbdb559e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/material-icon-search/precache-manifest.29030a524783f12c9c72f2e7ea1f6ad6.js"
+  "/material-icon-search/precache-manifest.04fe694aa9afcd257cb0b1cdc5ff6905.js"
 );
 
 self.addEventListener('message', (event) => {
